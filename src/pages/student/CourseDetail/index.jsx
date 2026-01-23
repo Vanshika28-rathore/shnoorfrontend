9fe1842985c7bb1e6b82884826ba9d1ca4291f79
syed643 <syed@shnoor.com>
2026-01-23 17:30:45 +0530

    c

--- src/pages/student/CourseDetail/index.jsx
+++ src/pages/student/CourseDetail/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { auth } from "../../auth/firebase";
+import { auth } from "../../../auth/firebase";
 import api from "../../../api/axios";
 import CourseDetailView from "./view";
 
